610e3d128802a6d75e213e344ad7e6e3e382f7af
Brandon Hubacher <brandon.hubacher@gmail.com>
2022-03-28 09:28:51 -0500

    wait for window to load on movie page

--- moviePage/movie.js
+++ moviePage/movie.js
@@ -1,20 +1,22 @@
-const BASE_URL = "https://polar-woodland-00821.herokuapp.com/";
 // window.onload = function () {
 //   var url = document.location.href;
 //   var imdbID = url.split("=")[1];
 //   console.log(imdbID);
 // };
 
-const url = document.location.href;
-const imdbID = url.split("=")[1];
-console.log(imdbID);
+window.onload = function () {
+  const BASE_URL = "https://polar-woodland-00821.herokuapp.com/";
+  const url = document.location.href;
+  const imdbID = url.split("=")[1];
+  console.log(imdbID);
 
-function getMovie(imdbID) {
-  return fetch(`${BASE_URL}movie/${imdbID}`).then((res) => res.json());
-}
+  function getMovie(imdbID) {
+    return fetch(`${BASE_URL}movie/${imdbID}`).then((res) => res.json());
+  }
 
-function showMovie(movie) {
-  console.log(movie);
-}
+  function showMovie(movie) {
+    console.log(movie);
+  }
 
-getMovie(imdbID).then(showMovie);
+  getMovie(imdbID).then(showMovie);
+};
