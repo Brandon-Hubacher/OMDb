cc8ed85b5d8c2733c7536c8fa07dc88b9c46997d
Brandon Hubacher <brandon.hubacher@gmail.com>
2022-05-01 17:08:28 -0500

    auth with database

--- auth/login/login.php
+++ auth/login/login.php
@@ -4,62 +4,46 @@
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  // Array of userid:password from passwd.txt
-  $loginsArr = file("../passwd.txt");
+  $db_server = 'spring-2022.cs.utexas.edu';
+  $db_username = 'cs329e_bulko_branhub';
+  $db_password = 'marble+dwell8Inform';
+  $db_dbName = 'cs329e_bulko_branhub';
 
-  if(!validLogIn($loginsArr, $username, $password))
-  {
-    /**
-     * I will note here an issue that I've been stuck with for a bit that is now resolved.
-     * 
-     * First, I noticed that the issue was not occurring when a proper username and password was entered.
-     * Whenever I would enter an incorrect username and/or password, I would get an error saying
-     * that $_POST didn't have those variables from the form. This was confusing because there was
-     * no issue when entering in valid information. I found that the error was in the code below.
-     * 
-     * Rather than redirecting the user to "./loginPage.php?error=1", I was redirecting them to
-     * "./login.php?error=1", the same php file that handles the processing rather than the actual page.
-     * So, it makes sense that $_POST was empty because no form was being submitted when using header().
-     */
-    header("Location: ./loginPage.php?error=1");
+  $mysqli = new mysqli($db_server, $db_username, $db_password, $db_dbName);
+
+  if($mysqli->connect_errno) {
+    die('CUSTOM Connect Error: ' . $mysqli->connect_errno . ": " . $mysqli-> connect_error);
+  }
+
+  // Create query to see if username is correct
+  $db_table = 'website_auth';
+  $query = "SELECT * FROM website_auth WHERE username = \"$username\"";
+
+  // Issue the query
+  $result = $mysqli->query($query);
+
+  // Verify the result
+  if (!$result) {
+    die("Query failed: ($db->error <br> SQL query = $query");
   }
   else
   {
-    //header("Location: ")
-    if(isset($_SESSION['current_url']))
+    $row = $result->fetch_assoc();
+    if($row['username'] != $username || $row['password'] != $password)
     {
-      //echo $_SESSION['current_url'];
-      setcookie("login", "testLogin", time()+120, "/");
-      header("Location: " . $_SESSION['current_url']);
+        header("Location: ./loginPage.php?error=1");
     }
     else
     {
-      header("Location: ../../homePage/homePage.php");
-    }
-  }
-
-
-  function validLogIn($loginsArr, $username, $password) {
-    $valid = true;
-
-
-    if(count($loginsArr) == 0) { $valid = false; }
-
-    if($valid)
-    {
-      $valid = false;
-      for($i = 0; $i < count($loginsArr); $i++)
+      if(isset($_SESSION['current_url']))
       {
-        $pair = explode(":", $loginsArr[$i]);
-
-        if($username == trim($pair[0]) && $password == trim($pair[1]))
-        {
-          $valid = true;
-          break;
-        }
+        setcookie("login", "testLogin", time()+120, "/");
+        header("Location: " . $_SESSION['current_url']);
+      }
+      else
+      {
+        header("Location: ../../homePage/homePage.php");
       }
     }
-
-    return $valid;
   }
-  ?>
+?>
--- auth/register/register.php
+++ auth/register/register.php
@@ -1,61 +1,50 @@
 <?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-    // Array of userid:password from passwd.txt
-    $loginsArr = file("../passwd.txt");
+  $db_server = 'spring-2022.cs.utexas.edu';
+  $db_username = 'cs329e_bulko_branhub';
+  $db_password = 'marble+dwell8Inform';
+  $db_dbName = 'cs329e_bulko_branhub';
 
-    // If valid username and password
-    // Username and password text validation must be done in js
-    if(!validSignUp($loginsArr, $username))
+  $mysqli = new mysqli($db_server, $db_username, $db_password, $db_dbName);
+
+  if($mysqli->connect_errno) {
+    die('CUSTOM Connect Error: ' . $mysqli->connect_errno . ": " . $mysqli-> connect_error);
+  }
+
+  // Create query to see if username already exists
+  $db_table = 'website_auth';
+  $query = "SELECT * FROM website_auth WHERE username = \"$username\"";
+
+  // Issue the query
+  $result = $mysqli->query($query);
+
+  // Verify the result
+  if (!$result) {
+    die("Query failed: ($db->error <br> SQL query = $query");
+  }
+  else
+  {
+    $row = $result->fetch_assoc();
+    if($row['username'] == $username)
     {
-      header("Location: ./registerPage.php?error=1");
+        header("Location: ./registerPage.php?error=1");
     }
     else
     {
-      /**
-       * Important note!
-       * 
-       * Writing an echo statement here and then trying to redirect user to login page
-       * was giving an error saying
-       * 
-       * Warning: Cannot modify header information - headers already sent by (line 24)
-       * in on line 35
-       */
-      // Open signup text file to write to it
-      $file = fopen("../passwd.txt", "a");
+      $query_add_user = "INSERT INTO website_auth VALUES (\"$username\", \"$password\")";
 
-      // Append sign up text file with updated info
-      fwrite($file, "\r\n" . $username . ":" . $password);
+      $result_add_user = $mysqli->query($query_add_user);
 
-      // Close signup text file
-      fclose($file);
-
-      header("Location: ../login/loginPage.php");
-      exit;
-    }
-
-
-    function validSignUp($loginsArr, $username) {
-      $valid = false;
-
-      if(count($loginsArr) == 0) { $valid = true; }
-
-      if(!$valid)
-      {
-        $valid = true;
-        for($i = 0; $i < count($loginsArr); $i++)
-        {
-          $pair = explode(":", $loginsArr[$i]);
-          if($username == $pair[0])
-          {
-            $valid = false;
-            break;
-          }
-          //fwrite($file, $textArr[$i] . "\r\n");
-        }
+      if (!$result_add_user) {
+        die("Query failed: ($db->error <br> SQL query = $query_add_user");
       }
-
-      return $valid;
+      else
+      {
+        header("Location: ../login/loginPage.php");
+        exit;
+      }
     }
-  ?>
+  }
+?>
